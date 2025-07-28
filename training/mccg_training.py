35e287c01feb770851debb48fb65b43886c5fe2a
Victorgonl <victorgonl@outlook.com>
2025-07-28 19:43:03 -0300

    Returning to unsupervised

--- training/mccg_training.py
+++ training/mccg_training.py
@@ -2,7 +2,6 @@ import time
 from datetime import datetime, timedelta
 
 import hdbscan
-from numpy import indices
 from sklearn.metrics.pairwise import pairwise_distances
 from dataset.enhance_graph import *
 from dataset.load_data import load_dataset, load_graph
@@ -46,20 +45,22 @@ class MCCG_Trainer:
         w_cluster,
         t_multiview,
         t_cluster,
-        refine, 
+        refine,
     ):
-        train_names, train_pubs = load_dataset("train")
-        eval_names, eval_pubs = load_dataset(mode)
 
-        for p, name in enumerate(train_names, 1):        
+        names, pubs = load_dataset(mode)
+        results = {}
+
+        for p, name in enumerate(names, 1):
             start_time = datetime.now()
             logger.info(
-                f"Training {p}/{len(train_names)}: {name} | Start Time: {start_time.strftime('%Y-%m-%d %H:%M:%S')}"
+                f"Training {p}/{len(names)}: {name} | Start Time: {start_time.strftime('%Y-%m-%d %H:%M:%S')}"
             )
-
             name_start = time.time()
 
-            label, ft_list, data = load_graph(name, "train", th_a, th_o, th_v)
+            results[name] = []
+
+            label, ft_list, data = load_graph(name, th_a, th_o, th_v)
             ft_list = ft_list.float().to(device)
             data = data.to(device)
             adj = get_adj(data.edge_index, data.num_nodes)
@@ -113,31 +114,27 @@ class MCCG_Trainer:
                 refine=refine,
             )
             model.to(device)
+
             optimizer = torch.optim.Adam(
                 model.parameters(), lr=args.lr, weight_decay=l2_coef
             )
 
             for epoch in range(1, args.epochs + 1):
-
                 model.train()
                 optimizer.zero_grad()
 
-
-                embd_multiview, embd_cluster = model(
-                    x1, adj1, M1, x2, adj2, M2
-                )
+                embd_multiview, embd_cluster = model(x1, adj1, M1, x2, adj2, M2)
 
                 dis = pairwise_distances(
                     embd_cluster.cpu().detach().numpy(), metric="cosine"
                 )
-                pred_labels = hdbscan.HDBSCAN(
+                labels = hdbscan.HDBSCAN(
                     cluster_selection_epsilon=db_eps,
                     min_samples=db_min,
                     min_cluster_size=db_min,
                     metric="precomputed",
                 ).fit_predict(dis.astype("double"))
-                
-                labels = torch.from_numpy(pred_labels).to(device)
+                labels = torch.from_numpy(labels).to(device)
 
                 loss_cluster = model.SelfSupConLoss(
                     embd_cluster.unsqueeze(1),
@@ -149,11 +146,7 @@ class MCCG_Trainer:
                     embd_multiview, labels, contrast_mode="all", temperature=t_multiview
                 )
 
-                w_multiview = 1 - w_cluster
-                loss_train = (
-                    w_cluster * loss_cluster
-                    + w_multiview * loss_multiview
-                )
+                loss_train = w_cluster * loss_cluster + (1 - w_cluster) * loss_multiview
 
                 loss_train.backward()
                 optimizer.step()
@@ -170,36 +163,30 @@ class MCCG_Trainer:
                         f"\n{'-'*100}"
                     )
 
-        eval_results = {}
-
-        for ename in eval_names:
-            _, eft_list, edata = load_graph(ename, mode, th_a, th_o, th_v)
-            eft_list = eft_list.float().to(device)
-            edata = edata.to(device)
-
-            adj_eval = get_adj(edata.edge_index, edata.num_nodes)
-            M_eval = get_M(adj_eval, t=2)
-
-            model.eval()
             with torch.no_grad():
-                embd = model.encoder(eft_list, adj_eval, M_eval)
+                model.eval()
+                embd = model.encoder(ft_list, adj, M)
                 embd = F.normalize(model.cluster_projector(embd), dim=1)
                 lc_dis = pairwise_distances(
                     embd.cpu().detach().numpy(), metric="cosine"
                 )
-                eval_labels = hdbscan.HDBSCAN(
+                labels = hdbscan.HDBSCAN(
                     cluster_selection_epsilon=db_eps,
                     min_samples=db_min,
                     min_cluster_size=db_min,
                     metric="precomputed",
                 ).fit_predict(lc_dis.astype("double"))
 
-                cm = torch.from_numpy(onehot_encoder(eval_labels))
-                soft_labels = torch.mm(cm, cm.t())
-                eval_results[ename] = matx2list(soft_labels)
+                class_matrix = torch.from_numpy(onehot_encoder(labels))
+                labels = torch.mm(class_matrix, class_matrix.t())
+                pred = matx2list(labels)
 
-        prediction = get_results(eval_names, eval_pubs, eval_results)
-        pre, rec, f1 = evaluate(prediction, args.ground_truth_file, print_names=True)
+                results[name] = pred
+
+        predict = get_results(names, pubs, results)
+
+        ground_truth = join(args.save_path, "src", args.mode, args.ground_truth_file)
+        pre, rec, f1 = evaluate(predict, ground_truth)
 
         logger.info(
             f"[{mode.upper()}] AVERAGE: Precision: {pre:.4f} | Recall: {rec:.4f} | F1: {f1:.4f}"
